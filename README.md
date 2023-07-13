8668d196a3211c007938c55d489a6bd472691602
Nathan Demick <n@demick.org>
2023-07-12 21:44:43 -0400

    allow dropping on foundation/piles

--- README.md
+++ README.md
@@ -22,3 +22,5 @@ HTML/CSS(?)/JS implementation of Solitaire. Using the classic Windows version as
 - [x] get the proportions right, and see how playable it is on a smaller phone in portrait mode
   * can possibly make the cards a bit larger (currently 75x100)
 - [ ] placeholder images for card fronts
+- [ ] draw cards in piles offset
+- [ ] allow picking up cards in piles
---
+++ scripts/card.js
@@ -0,0 +1,29 @@
+class Card {
+  faceUp = false;
+  x = 0;
+  y = 0;
+  backImg = null;
+  frontImg = null;
+  suite = null;
+  rank = null;
+
+  // TODO: figure out a way to link stacked cards together
+  parent = null;
+  child = null;
+
+  constructor(rank, suite, images) {
+    this.rank = rank;
+    this.suite = suite;
+
+    this.backImg = images['backs_one'];
+    this.frontImg = images[`${this.suite}_${this.rank}`];
+  }
+
+  get image() {
+    if (this.faceUp) {
+      return this.frontImg;
+    }
+
+    return this.backImg;
+  }
+}
