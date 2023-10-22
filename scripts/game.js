aaac8233f804d3da89ad7952af1a1398715db975
endemic <n@demick.org>
2023-10-22 14:08:02 -0400

    remove `card_actions.js` src file

--- scripts/game.js
+++ scripts/game.js
@@ -13,6 +13,27 @@ const onImageLoad = e => {
   }
 }
 
+// convert global browser coordinates to canvas coordinates
+const getCoords = event => {
+  // need to scale the touches by the actual size of the canvas;
+  // e.g. the canvas still thinks it is 605px wide even if it is
+  // scaled to 400px by CSS rules
+  const scale = event.target.width / event.target.clientWidth;
+
+  if (event.changedTouches && event.changedTouches.length > 0) {
+    return {
+      x: event.changedTouches[0].clientX * scale,
+      y: event.changedTouches[0].clientY * scale
+    };
+  }
+
+  // this seems to translate to <canvas> coordinates
+  return {
+    x: event.x - event.target.offsetLeft,
+    y: event.y - event.target.offsetTop
+  }
+};
+
 // enumerate over image sources, and load them into memory
 IMG_SRC.forEach(src => {
   // key is in format `suit_rank` -- probably a better way to do this
