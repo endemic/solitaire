978638fc4fbb15edeea05b28054c4673958cf897
endemic <n@demick.org>
2023-11-19 23:34:36 -0500

    Draw status bar using scaled coords

--- scripts/status-bar.js
+++ scripts/status-bar.js
@@ -19,25 +19,33 @@ export default class StatusBar {
     // TODO: dynamically adjust font size; base height of status bar off minimum font size
     // make minimum font size readable on portrait phone
 
-    const windowMargin = (this.canvas.width - this.width) / 2;
     const text = `Score: ${this.score}  Time: ${this.time}`;
     const textSize = this.context.measureText(text);
     const rightPadding = this.height * 0.25; // used for some padding on the right side of the score/timer
 
+    // need to account for scaling when displayed on HiDPI screens
+    const scaledCanvasWidth = parseInt(this.canvas.style.width, 10);
+    const scaledCanvasHeight = parseInt(this.canvas.style.height, 10);
+
+    const windowMargin = (scaledCanvasWidth - this.width) / 2;
+
     // draw top border
     this.context.fillStyle = 'black';
-    this.context.fillRect(0, this.canvas.height - this.height, this.canvas.width, this.borderSize);
+    this.context.fillRect(0, scaledCanvasHeight - this.height, scaledCanvasWidth, this.borderSize);
+
+    // hidpi: 628px, 1884
+    // alert(`${this.canvas.style.height}, ${this.canvas.height}`);
 
     // draw white stats bar background
     this.context.fillStyle = 'white';
-    this.context.fillRect(0, this.canvas.height - this.height + this.borderSize, this.canvas.width, this.height);
+    this.context.fillRect(0, scaledCanvasHeight - this.height + this.borderSize, scaledCanvasWidth, this.height);
 
     // set text style
     this.context.font = `${this.fontSize}px "Generic Mobile System", monospace`;
     this.context.fillStyle = 'black';
 
     //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_text
-    this.context.fillText(text, windowMargin + this.width - textSize.width - rightPadding, this.canvas.height - textSize.fontBoundingBoxDescent);
+    this.context.fillText(text, windowMargin + this.width - textSize.width - rightPadding, scaledCanvasHeight - textSize.fontBoundingBoxDescent);
   }
 
   startTimer() {
