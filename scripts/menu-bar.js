6a8381abcb81e8acd90ba31f747d79229d91cd7b
endemic <n@demick.org>
2023-12-10 21:49:17 -0500

    WIP draw top menu

--- scripts/menu-bar.js
+++ scripts/menu-bar.js
@@ -1,8 +1,10 @@
 export default class MenuBar {
   // internal sizing var that get set in `resize`
   width = null;
+  height = null;
 
   borderSize = 1;
+  minFontSize = 16;
 
   constructor(canvas) {
     // dunno if we need both or not
@@ -11,50 +13,69 @@ export default class MenuBar {
 
     // add event listeners to listen for menu events
 
+    // todo: might have to pass these options in as arguments to the constructor
+    // each top level menu item needs to calculate its own dimensions (with padding, etc) to allow
+    // for easy collision detection
+    // each top menu item also needs to calculate the overall size of its child menu dropdown
     this.items = [
-      'Menu',
-      'Game'
+      {
+        text: 'Game',
+        items: [
+          { text: 'Deal', callback: () => {/* deal new game */} },
+          { text: 'Draw Three', selected: true, callback: () => {/* toggle between draw 1/3 */} },
+          { text: 'Sound', selected: true, callback: () => {/* toggle playing sound */} },
+        ]
+      },
+      {
+        text: 'Help',
+        items: [
+          { text: 'How to Play', callback: () => {/* go to wiki */} },
+          { text: 'About...', callback: () => {/* somewhere */} },
+        ]
+      }
     ];
   }
 
   draw() {
-    return;
-    // TODO: dynamically adjust font size; base height of status bar off minimum font size
-    // make minimum font size readable on portrait phone
-    let fontSize = this.width * 0.025;
-    const minFontSize = 16;
-    const windowMargin = (this.canvas.width - this.width) / 2;
-    const text = 'Menu  Game';
-    const textSize = this.context.measureText(text);
+    // need to account for scaling when displayed on HiDPI screens
+    const scaledCanvasWidth = parseInt(this.canvas.style.width, 10);
+    // const scaledCanvasHeight = parseInt(this.canvas.style.height, 10);
 
-    fontSize = fontSize < minFontSize ? minFontSize : fontSize;
+    const windowMargin = (scaledCanvasWidth - this.width) / 2;
 
-    const height = fontSize * 1.2; // height of status bar
-    const margin = height * 0.25; // used for some margin on the right side of the score/timer
-
-    // draw top border
+    // draw bottom border
     this.context.fillStyle = 'black';
-    this.context.fillRect(0, height + this.borderSize, this.canvas.width, this.borderSize);
+    this.context.fillRect(0, this.height + this.borderSize, scaledCanvasWidth, this.borderSize);
 
-    // draw white stats bar background
+    // draw white background
     this.context.fillStyle = 'white';
-    this.context.fillRect(0, 0, this.canvas.width, height);
+    this.context.fillRect(0, 0, scaledCanvasWidth, this.height);
 
     // set text style
-    // https://en.wikipedia.org/wiki/System_(typeface)
-    // https://www.dafont.com/w95fa.font
-    // https://www.dafont.com/generic-mobile-system.font
-    this.context.font = `${fontSize}px Arial, sans-serif`;
+    this.context.font = `${this.fontSize}px "Generic Mobile System", monospace`;
     this.context.fillStyle = 'black';
 
+    // TODO determine which menu item is open, based on clix
+    const text = this.items.map(item => item.text).join(' ');
+    const textSize = this.context.measureText(text);
+
+    console.log(textSize);
+
     //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_text
-    this.context.fillText(text, windowMargin + margin, textSize.fontBoundingBoxAscent);
+    this.context.fillText(text, windowMargin, textSize.actualBoundingBoxAscent + textSize.actualBoundingBoxDescent);
   }
 
   resize(tableauWidth) {
     this.width = tableauWidth;
 
-    // TODO: move sizing nonsense in here so height can be accessed externally
+    this.fontSize = this.width * 0.025;
+
+    if (this.fontSize < this.minFontSize) {
+      this.fontSize = this.minFontSize;
+    }
+
+    this.height = this.fontSize * 1.2;
+
 
     this.draw();
   }
